73dbc9a2a097cab0c4dd120035ea486560f4b919
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-07-30 11:14:22 -0300

    tentando exibir os itens

--- assets/js/services/animais.js
+++ assets/js/services/animais.js
@@ -1,8 +1,23 @@
-const listaAnimais = () => fetch("http://localhost:3000/cachorros").then(resposta => resposta.json())
+const listaAnimais = () => fetch("http://localhost:3000/cachorros", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }
+}).then(resposta => resposta.json())
 
-const listaGatos = () => fetch("http://localhost:3000/gatos").then(resposta => resposta.json())
+const listaGatos = () => fetch("http://localhost:3000/gatos", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }
+}).then(resposta => resposta.json())
 
-const listaCoelhos = () => fetch("http://localhost:3000/coelhos").then(resposta => resposta.json())
+const listaCoelhos = () => fetch("http://localhost:3000/coelhos", {
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }
+}).then(resposta => resposta.json())
 
 export const animaisServices = {
     listaAnimais,

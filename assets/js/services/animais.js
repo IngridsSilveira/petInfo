a4d222c05c4b27d8995909c2c2a1a161d930121a
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-07-30 11:35:46 -0300

    tentativa 2

--- assets/js/services/animais.js
+++ assets/js/services/animais.js
@@ -1,23 +1,87 @@
-const listaAnimais = () => fetch("http://localhost:3000/cachorros", {
+
+
+/*const listaAnimais = () => fetch("http://localhost:3000/cachorros", {
     method: "GET",
     headers: {
         "Content-Type": "application/json"
     }
-}).then(resposta => resposta.json())
+}).then(resposta => resposta.json());
 
 const listaGatos = () => fetch("http://localhost:3000/gatos", {
     method: "GET",
     headers: {
         "Content-Type": "application/json"
     }
-}).then(resposta => resposta.json())
+}).then(resposta => resposta.json());
 
 const listaCoelhos = () => fetch("http://localhost:3000/coelhos", {
     method: "GET",
     headers: {
         "Content-Type": "application/json"
     }
-}).then(resposta => resposta.json())
+}).then(resposta => resposta.json());
+*/
+
+async function listaAnimais() {
+    try {
+      const response = await fetch('http://localhost:3000/cachorros', {
+        method: 'GET',
+        headers: {
+          accept: 'application/json'
+        }
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  
+async function listaGatos() {
+    try {
+      const response = await fetch('http://localhost:3000/gatos', {
+        method: 'GET',
+        headers: {
+          accept: 'application/json'
+        }
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+async function listaCoelhos() {
+    try {
+      const response = await fetch('http://localhost:3000/coelhos', {
+        method: 'GET',
+        headers: {
+          accept: 'application/json'
+        }
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error! status: ${response.status}`);
+      }
+  
+      const result = await response.json();
+      return result;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
 
 export const animaisServices = {
     listaAnimais,

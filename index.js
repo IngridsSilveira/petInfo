53debb958a959d6f1b474ba2d2aadf2e336d4fc9
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-08-02 09:51:15 -0300

    tentativa 4

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors('*'));
 
 app.get('/', (req, res) => {
-    //res.set('Acess-Controll-Allow-Origin', 'https://ingridssilveira.github.io/petInfo/');
+    res.set('Acess-Controll-Allow-Origin', 'https://ingridssilveira.github.io/petInfo/');
     res.send("ok");
 });
 

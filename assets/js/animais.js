92259745ff336b2a660c6020fc0ff774a2d04c59
Ingrid Souza <92730473+IngridsSilveira@users.noreply.github.com>
2022-08-04 11:51:10 -0300

    projeto finalizado

--- assets/js/animais.js
+++ assets/js/animais.js
@@ -1,77 +1,115 @@
-const cachorrosURL = "db.json";
-const gatosURL = "db.json";
-const coelhosURL = "db.json";
+const URL = 'https://my-json-server.typicode.com/IngridsSilveira/petInfo/db';
 
-const container = document.querySelector('[data-container]')
-const container2 = document.querySelector('[data-container2]')
-const container3 = document.querySelector('[data-container3]')
+const container = document.querySelector('[data-container]');
+const container2 = document.querySelector('[data-container2]');
+const container3 = document.querySelector('[data-container3]');
 
-  fetch(cachorrosURL).then(response => {
+
+  fetch(URL).then(response => {
     return response.json();
   })
   .then(data => {
     const html = data.cachorros.
     map(element => {
       return `
-      <div class="conteudo">
+      <div class="conteudo" ${element.id}>
         <img class="imagens" src="${element.image}"/>
         <h3 class="titles" data-nome>${element.nome}</h3>
         <p class="descricao">${element.descricao}</p>
       </div>
       `
-    })
+    }).join("");
     container.innerHTML = html;
-  })
+  });
 
-  fetch(gatosURL).then(response => {
+  fetch(URL).then(response => {
     return response.json();
   })
   .then(data => {
     const html = data.gatos.
     map(element => {
       return `
-      <div class="conteudo">
+      <div class="conteudo" ${element.id}>
         <img class="imagens" src="${element.image}"/>
         <h3 class="titles" data-nome>${element.nome}</h3>
         <p class="descricao">${element.descricao}</p>
       </div>
       `
-    });
+    }).join("");
     container2.innerHTML = html;
-  })
+  });
 
-  fetch(coelhosURL).then(response => {
+  fetch(URL).then(response => {
       return response.json();
     })
     .then(data => {
       const html = data.coelhos.
       map(element => {
         return `
-        <div class="conteudo">
+        <div class="conteudo" ${element.id}>
             <img class="imagens" src="${element.image}"/>
             <h3 class="titles" data-nome>${element.nome}</h3>
             <p class="descricao">${element.descricao}</p>
         </div>
       `
-      });
+      }).join("");
       container3.innerHTML = html;
 
-      const nomes = document.querySelectorAll("[data-nome]");
+      const pesquisa = document.querySelector(".input_search");
 
-      const botao = document.querySelector(".search-btn")
+      pesquisa.addEventListener("keyup", function(e){
+        let value = e.target.value;
 
-      botao.addEventListener("click", function(){
-      let input = document.querySelector('.input_search').value
-      input = input.toLowerCase();
-      
-      for (i = 0; i < nomes.length; i++) { 
-          if (!nomes[i].innerHTML.toLowerCase().includes(input)) {
-              nomes[i].style.backgroundColor = '';
+        let nomes = document.querySelectorAll("[data-nome]")
+
+        for (i = 0; i < nomes.length; i++) { 
+          if (!nomes[i].innerHTML.toLowerCase().includes(value)) {
           }
-          else {
-              nomes[i].style.backgroundColor = '#ccc';
+          else if(value == "rex"){
+            nomes[i].style.color = "blue";
           }
-      }
-      return
+          else if(value == "doguito"){
+            nomes[i].style.color = "pink";
+          }
+          else if(value == "split"){
+            nomes[i].style.color = "green";
+          }
+          else if(value == "bambi"){
+            nomes[i].style.color = "red";
+          }
+          else if(value == "harry"){
+            nomes[i].style.color = "orange";
+          }
+          else if(value == "luck"){
+            nomes[i].style.color = "brown";
+          }
+          else if(value == "buck"){
+            nomes[i].style.color = "lime";
+          }
+          else if(value == "stonk"){
+            nomes[i].style.color = "turquoise";
+          }
+          else if(value == "john"){
+            nomes[i].style.color = "cadetblue";
+          }
+          else if(value == "ralf"){
+            nomes[i].style.color = "purple";
+          }
+          else if(value == "sony"){
+            nomes[i].style.color = "yellow";
+          }
+          else if(value == "snook"){
+            nomes[i].style.color = "lightgreen";
+          }
+          else if(value == "henry"){
+            nomes[i].style.color = "teal";
+          }
+          else if(value == "sid"){
+            nomes[i].style.color = "violet";
+          }
+          else if(value == "tony"){
+            nomes[i].style.color = "coral";
+          }
+        }
       })
-  })
+  });
--- assets/js/estilos.js
+++ assets/js/estilos.js
@@ -3,23 +3,19 @@ const gatos = document.querySelector('[data-cat]')
 const coelhos = document.querySelector('[data-rabbit]')
 const todos = document.querySelector('[data-todos-animais]')
 
-todos.addEventListener('click', () => {
-    const background = document.querySelector('.background');
-    const texto = document.querySelector('.sub_titulo');
+const background = document.querySelector('.background');
+const texto = document.querySelector('.sub_titulo');
+const conteudoCachorros = document.querySelector('[data-conteudo-cachorros]')
+const conteudoGatos = document.querySelector('[data-conteudo-gatos]')
+const conteudoCoelhos = document.querySelector('[data-conteudo-coelhos]')
 
-    
+todos.addEventListener('click', () => {
 
     texto.innerHTML = "Todos os animais";
 
     background.style.backgroundImage = "url(assets/images/background.jpeg)";
     background.style.backgroundSize = "contain";
     background.style.backgroundPosition = "center";
-
-
-
-    const conteudoGatos = document.querySelector('[data-conteudo-gatos]')
-    const conteudoCoelhos = document.querySelector('[data-conteudo-coelhos]')
-    const conteudoCachorros = document.querySelector('[data-conteudo-cachorros]')
 
     conteudoGatos.style.display = "flex";
     conteudoCoelhos.style.display = "flex";
@@ -28,12 +24,6 @@ todos.addEventListener('click', () => {
 
 
 cachorros.addEventListener('click', () => {
-    //SELECIONA OS ITENS//
-    const background = document.querySelector('.background');
-    const texto = document.querySelector('.sub_titulo');
-    const conteudoGatos = document.querySelector('[data-conteudo-gatos]')
-    const conteudoCoelhos = document.querySelector('[data-conteudo-coelhos]')
-    const conteudoCachorros = document.querySelector('[data-conteudo-cachorros]')
 
     //MUDA O ESTILO DO BACKGROUND//
     texto.innerHTML = "Cachorros";
@@ -49,12 +39,6 @@ cachorros.addEventListener('click', () => {
 
 
 gatos.addEventListener('click', () => {
-    const background = document.querySelector('.background');
-    const texto = document.querySelector('.sub_titulo');
-    const conteudoCachorros = document.querySelector('[data-conteudo-cachorros]')
-    const conteudoCoelhos = document.querySelector('[data-conteudo-coelhos]')
-    const conteudoGatos = document.querySelector('[data-conteudo-gatos]')
-
     texto.innerHTML = "Gatos";
     background.style.backgroundImage = "url(assets/images/cats-bg.jpeg)";
     background.style.backgroundSize = "cover";
@@ -67,12 +51,6 @@ gatos.addEventListener('click', () => {
 
 
 coelhos.addEventListener('click', () => {
-    const background = document.querySelector('.background');
-    const texto = document.querySelector('.sub_titulo');
-    const conteudoCachorros = document.querySelector('[data-conteudo-cachorros]')
-    const conteudoGatos = document.querySelector('[data-conteudo-gatos]')
-    const conteudoCoelhos = document.querySelector('[data-conteudo-coelhos]')
-
     texto.innerHTML = "Coelhos";
     background.style.backgroundImage = "url(assets/images/rabbit-bg.jpeg)";
     background.style.backgroundPosition = "bottom";
